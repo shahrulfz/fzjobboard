2c71298ab3c7c3ca1f203badea260d7e46fe4e1d
Shahrul Fz <shahrulefz@gmail.com>
2020-03-30 18:16:51 +0800

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('Express');
 var app = express();
 var bodyParser = require('body-parser')
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -41,5 +41,5 @@ app.get('*', function(req, res){
 
 // app.listen(3000);
 app.listen(port, function () {
-   console.log(`Example app listening on port !`);
+   console.log(`Example app listening on port ! ${port}`);
 });
